f93d568977181ddd8f9e87ca7539f34e042e245e
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-02-16 01:31:11 +0700

    start SearchBar component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,6 +3,17 @@ import './App.css';
 
 export class App extends React.Component {
     render() {
-        return <h1>Test</h1>
+        return (
+            <div>
+                <h1>Ja<span className="highlight">mmm</span>ing</h1>
+                <div className="App">
+                    {/* <!-- Add a SearchBar component --> */}
+                    <div className="App-playlist">
+                        {/* <!-- Add a SearchResults component --> */}
+                        {/* <!-- Add a Playlist component --> */}
+                    </div>
+                </div>
+            </div>
+        )
     }
 };
