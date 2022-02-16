0c0accf44a5a5a0ff021460289b63808af96747a
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-02-16 17:06:01 +0700

    refactor conditional in renderAction

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -7,7 +7,7 @@ export class Track extends React.Component {
             <div className="Track">
                 <div className="Track-information">
                     {/* <!-- track name will go here --> */}
-                    <h3></h3>
+                    <h3>Track name will go here</h3>
                     {/* <!-- track artist will go here--> | <!-- track album will go here --> */}
                     <p></p>
                 </div>
@@ -18,10 +18,8 @@ export class Track extends React.Component {
     };
     // Method displays a <button> element with - as its content if the isRemoval property is true, and a + <button> element if the isRemoval property is false. Set the class name to Track-action.
     renderAction() {
-        return (
-            (this.props.isRemoval ?
-                <button className="Track-action">-</button> :
-                <button className="Track-action">+</button>)
-        );
+        this.props.isRemoval ?
+            <button className="Track-action">-</button> :
+            <button className="Track-action">+</button>
     }
 };
